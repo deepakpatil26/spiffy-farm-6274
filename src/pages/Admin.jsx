bdbbb7415511c83aab121f15af16f9bbee1da263
Deepak Patil <deepakpatil2612@gmail.com>
2025-01-01 10:58:29 +0530

    fix: deployment related changes and ErrorBoundary fix

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,17 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React, { ErrorBoundary } from "react";
+import React from "react";
 import { Box } from "@chakra-ui/react";
 import AdminNavbar from "../Components/Admin/AdminNavbar";
 import AdminSidebar from "../Components/Admin/AdminSidebar";
 
 function Admin() {
   return (
-    <ErrorBoundary>
-      <Box>
-        <AdminNavbar />
-        <AdminSidebar />
-      </Box>
-    </ErrorBoundary>
+    <div>
+      <AdminNavbar />
+      <AdminSidebar />
+    </div>
   );
 }
 
